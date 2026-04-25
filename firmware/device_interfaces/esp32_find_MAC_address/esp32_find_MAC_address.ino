ddedc1f285e5ef289e6db3271c2efce1a4cb4d09
Luke Melikian <lhmelikian@gmail.com>
2026-04-25 09:56:33 -0700

    changed wifi mac address reader to ble mac address reader (they are different)

--- firmware/device_interfaces/esp32_find_MAC_address/esp32_find_MAC_address.ino
+++ firmware/device_interfaces/esp32_find_MAC_address/esp32_find_MAC_address.ino
@@ -1,12 +1,16 @@
-#include "WiFi.h"
+#include "BLEDevice.h"
 
 void setup() {
   delay(2000);
+  BLEDevice::init("");
+
   Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
+  // WiFi.mode(WIFI_STA);
 }
 
 void loop() {
-  Serial.print("ESP MAC Address: ");
-  Serial.println(WiFi.macAddress());
+  // Serial.print("ESP MAC Address: ");
+  // Serial.println(WiFi.macAddress());
+  Serial.print("ESP BLE Address: ");
+  Serial.println(BLEDevice::getAddress().toString().c_str());
 }
